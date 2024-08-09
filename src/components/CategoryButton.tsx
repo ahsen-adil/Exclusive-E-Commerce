12d957630372967090f514a98e11f4b7995f9cf0
Bioib <revandvoey@gmail.com>
2024-08-10 04:40:03 +0700

    feat(components): add `CategoryButton`

---
+++ src/components/CategoryButton.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+type Props = {
+  children?: React.ReactNode;
+  name?: string;
+  icon?: React.ReactNode;
+  href?: string;
+};
+
+const CategoryButton = (props: Props) => {
+  return (
+    <Link
+      href={props.href || "#"}
+      className="hover:bg-red flex h-36 w-44 flex-col items-center justify-center space-y-4 rounded-md border border-black border-opacity-30 px-14 py-6 transition-colors duration-300 hover:text-text"
+    >
+      <div className="">{props.icon}</div>
+      <p className="text-center">{props.name}</p>
+    </Link>
+  );
+};
+
+export default CategoryButton;

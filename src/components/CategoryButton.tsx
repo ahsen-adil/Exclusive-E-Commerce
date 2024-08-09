4e56165c821416b2c7b311c540116d65a942b093
Bioib <revandvoey@gmail.com>
2024-08-10 04:51:24 +0700

    chore: remove border on hover

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -11,7 +11,7 @@ const CategoryButton = (props: Props) => {
   return (
     <Link
       href={props.href || "#"}
-      className="hover:bg-red flex h-36 w-44 flex-col items-center justify-center space-y-4 rounded-md border border-black border-opacity-30 px-14 py-6 transition-colors duration-300 hover:text-text"
+      className="hover:bg-red flex h-36 w-44 flex-col items-center justify-center space-y-4 rounded-md border border-black border-opacity-30 px-14 py-6 transition-colors duration-300 hover:border-none hover:text-text"
     >
       <div className="">{props.icon}</div>
       <p className="text-center">{props.name}</p>
